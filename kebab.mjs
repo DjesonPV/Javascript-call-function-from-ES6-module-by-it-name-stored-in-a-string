ca9674a9ebdf145fe97fc0419e9a0f640a2e1c44
DjesonPV <djeson.pascalvalette@gmail.com>
2022-05-01 00:51:19 +0200

    Works as ES6 modules
    When you want to call an imported function with its name stringifed.
    
    In my case, it's for a Discord.js Bot that check messages for a command. Thus I have the function name in a string.

---
+++ kebab.mjs
@@ -0,0 +1,16 @@
+
+export function salade(){
+
+    console.log("salade");
+};
+
+export function tomate(n){
+
+     console.log("tomate - " + n);
+};
+
+function oignon () {
+
+    console.log("CHEESE");
+};
+
---
+++ main.mjs
@@ -0,0 +1,9 @@
+import * as kebab from './kebab.mjs';
+
+    kebab["salade"]();
+    kebab["tomate"](1);
+    kebab["tomate"](2);
+    kebab["tomate"](3);
+
+
+
